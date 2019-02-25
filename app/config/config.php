c2b9b986d9f67c17de7edfc2273cc73c60f77476
Ignacio Morinigo <morinacho@gmail.com>
2019-02-25 12:23:39 -0300

    I add the configuration of the database and the constants of app, url and the site name

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php  
+
+	#Data base access config
+	define('DB_HOST', '_HOST_'); 	 # Put the name of the host
+	define('DB_USER', '_USER_'); 	 # Put the username
+	define('DB_PASSWORD', '_PASS_'); # Put the password
+	define('DB_NAME', 'test');		 # Put the data base name
+	
+	# App route
+	define('APP_ROUTE', dirname(dirname(__FILE__)));
+
+	# URL route
+	define('URL_ROUTE', '_URLROUTE_'); # Put the url route of your site
+
+	# Site name
+	define('SITENAME', '_SITENAME_');  # Put the name of the site
+	
+?>
