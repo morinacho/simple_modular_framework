f1b8e879a99a2c737d2e55782357e20d03cfd1de
Ignacio Morinigo <morinacho@gmail.com>
2020-09-03 16:29:41 -0300

    Role fix

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,21 +4,8 @@
 		
 		public function __construct() {}
 
-		public function index(){
-			if(!isset($_SESSION['role'])){
-				$this->view('index'); 
-			}
-			else{
-				echo $_SESSION['role'];
-				switch($_SESSION['role']){
-					case 1:
-						$this->view('admin/index');
-						break;
-					case 2:
-						$this->view('user/index');
-						break; 
-				}  
-			} 
+		public function index(){ 
+			$this->view('index');  
 		}
 	} 
 ?>
--- app/views/index.php
+++ app/views/index.php
@@ -1,7 +1,15 @@
 <?php 
 
 	require_once APP_ROUTE . '/views/modules/header.php'; 
-	require_once APP_ROUTE . '/views/public/home.php';
+	switch ($_SESSION['role']){
+		case 1:
+			require_once APP_ROUTE . '/views/admin/index.php';
+			break;
+		case 2: 
+			require_once APP_ROUTE . '/views/user/index.php';
+			break;
+		default:
+			require_once APP_ROUTE . '/views/public/home.php';
+	}	
 	require_once APP_ROUTE . '/views/modules/footer.php'; 
-
 ?>
