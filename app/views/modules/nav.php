073d098889f53bd73c26f8692ae742bf45df21a3
Ignacio Morinigo <morinacho@gmail.com>
2020-09-05 14:24:33 -0300

    Refactor

--- app/views/modules/nav.php
+++ app/views/modules/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="<?php echo URL_ROUTE ?>home">
     <img src="<?php echo URL_ROUTE ?>/media/system/icons/logo.png" alt="">
-    SMF v1
+    SMF v2
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
--- app/views/public/home.php
+++ app/views/public/home.php
@@ -4,7 +4,7 @@
 	<div class="col-12 text-center mt-5">
 		<h3>Welcome to Simple Modular Framework</h3>
 		<h4>MVC POO PDO</h4>
-		<p>Version 1.1.0</p>
+		<p>Version 2.0.0</p>
 		<small>Public user</small>
 	</div>
 
