9acf433bb97d6e829589f0355a105208a6c35370
Ignacio Morinigo <morinacho@gmail.com>
2023-10-25 15:46:28 -0300

    fix: main views!

--- app/entities/auth/controller/Auth.php
+++ app/entities/auth/controller/Auth.php
@@ -5,9 +5,35 @@
 
     class Auth extends Controller{
         
+        private $userModel;
 
-        public static function authenticated(){
-			return (isset($_SESSION['username']));
+        public function __construct(){
+			
+			session_start(); 
 		}
+
+        public function login(){
+			if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])){
+				if(isset($_POST['user-email']) && isset($_POST['user-password'])){
+					$email = $_POST['user-email'];
+					$pass  = $_POST['user-password'];
+					$user  = $this->userModel->getByEmail($email);
+					
+					if(!empty($user) && password_verify($pass, $user->contrasena)){
+						$_SESSION['username'] = "$user->nombres $user->apellido";
+						$_SESSION['rol']      = $user->rol_id;
+						$_SESSION['userdoc']  = $user->documento;
+						header("Location:".URL_ROUTE);	
+					}
+				}
+			} 		
+		} 
+
+        public function logout(){
+			session_unset();
+            session_destroy(); 
+		}
+
+        
     }
 ?>
